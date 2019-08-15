6f52621a8f4b7e43413f1638b990c3b3781842fa
RedPlus <redplus@redplus.net>
2019-08-16 03:52:01 +0900

    선택정렬알고리즘

---
+++ C/선택정렬알고리즘/선택정렬알고리즘.c
@@ -0,0 +1,35 @@
+/*
+	선택 정렬 알고리즘 : Selection Sort : 가장 작거나 큰 데이터를 왼쪽으로 밀어서 정렬시키는 방법
+		ASCending : 오름차순 : 1, 2, 3 순, A, B, C 순
+		DESCending : 내림차순 : 321 순, 다나가 순
+*/
+#include <stdio.h>
+#define N 5
+
+void main(void)
+{
+	//[0] Init
+	int i, j, temp;
+	//[1] Input
+	int data[N] = { 3, 2, 1, 5, 4 };
+	temp = 0;
+	//[2] Process : 알고리즘(문제 해결 능력) : for+if
+	for (i = 0; i < N - 1; i++)
+	{
+		for (j = i + 1; j < N; j++)
+		{
+			if (data[i] > data[j])
+			{
+				temp = data[i];
+				data[i] = data[j];
+				data[j] = temp;
+			}
+		}
+	}
+	//[3] Output
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d\t", data[i]);
+	}
+	printf("\n");
+}
