a036421cee1f1271bc769da745f0ab2b56bf77ec
RedPlus <redplus@redplus.net>
2019-08-16 03:17:52 +0900

    GetSumTwoNumber

---
+++ C/FunctionReturnValue/FunctionReturnValue.c
@@ -0,0 +1,15 @@
+// 반환값이 있는 함수(메서드)
+#include <stdio.h>
+
+int SquareFunction(int x)
+{
+	int r = x * x;
+	return r;
+}
+
+int main()
+{
+	printf("%d\n", SquareFunction(2)); // 4
+	printf("%d\n", SquareFunction(4)); // 16
+	return 0;
+}
