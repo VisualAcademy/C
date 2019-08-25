73553e1151f5b87f81dc9d1862e0cab513ff9cc3
RedPlus <redplus@redplus.net>
2019-08-26 03:06:03 +0900

    검색 알고리즘 강의 소스 추가

---
+++ C/BooleanDemo/BooleanDemo.c
@@ -0,0 +1,12 @@
+//[?] 논리 자료형: 참(True) 또는 거짓(False) 값 저장
+#include <stdio.h>
+#include <stdbool.h> // bool, true(1), false(0)
+
+main()
+{
+	bool bln = true; // 참(1)
+	printf("%d\n", bln); // 1
+
+	bool isFalse = false; // 거짓(0)
+	printf("%d\n", isFalse); // 0
+}
