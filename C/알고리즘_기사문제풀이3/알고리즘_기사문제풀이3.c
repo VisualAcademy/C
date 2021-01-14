24a1c0b2be5c78ddbb7b75e22483a8fca9e17abd
RedPlus <redplus@redplus.net>
2021-01-15 01:22:09 +0900

    알고리즘_기사문제풀이3.c

---
+++ C/알고리즘_기사문제풀이3/알고리즘_기사문제풀이3.c
@@ -0,0 +1,23 @@
+//1/2 - 2/3 + 3/4 - 4/5 +.........+ 99/100
+#include <stdio.h>
+
+void main(void)
+{
+    // Input 
+    double dblSum = 0; // 결과값
+    int i = 0; // 반복용
+    // Process
+    for (i = 1; i <= 99; i++)
+    {
+        if (i % 2 == 1) // 홀수
+        {
+            dblSum += i / (double)(i + 1);
+        }
+        else
+        {
+            dblSum += -i / (double)(i + 1);
+        }
+    }
+    // Output
+    printf("결과 : %.2f\n", dblSum);//0.69
+}
