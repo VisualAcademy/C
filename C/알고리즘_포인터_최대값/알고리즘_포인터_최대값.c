2dfff99cf4035c9d52abe6c65dec4439ce4b558d
RedPlus <redplus@redplus.net>
2021-01-20 08:44:30 +0900

    알고리즘_포인터_최대값.c

---
+++ C/알고리즘_포인터_최대값/알고리즘_포인터_최대값.c
@@ -0,0 +1,22 @@
+// 최대값을 구하는 알고리즘
+#include <stdio.h>
+#include <limits.h> // INT_MIN, INT_MAX
+
+void main()
+{
+	//[1] Input
+	int intNum[] = { -3, -2, -1, -4, -5 };
+	int intMax = INT_MIN;
+	int i;
+	int* p; p = intNum;//&intNum[0];
+	//[2] Process : MAX
+	for (i = 0; i < 5; i++)
+	{
+		if (intMax < *(p + i))
+		{
+			intMax = *(p + i);
+		}
+	}
+	//[3] Output
+	printf("최대값 : %d\n", intMax);
+}
