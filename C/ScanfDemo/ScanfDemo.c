0b787a562d251dfba6c0d6db156f4b4ec1945de2
RedPlus <redplus@redplus.net>
2021-01-22 19:48:10 +0900

    ScanfDemo

---
+++ C/ScanfDemo/ScanfDemo.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int age = 0;
+
+	// printf() 출력문
+	printf("나이는? ");
+
+	// scanf() 입력문
+	scanf("%d", &age);
+
+	printf("%d\n", age);
+	system("pause");
+	return 0;
+}
+
