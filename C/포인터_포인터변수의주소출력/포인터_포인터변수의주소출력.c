63b2ebeccae212ef0d583946a12fb9e0d5727544
RedPlus <redplus@redplus.net>
2021-01-28 04:04:59 +0900

    포인터_포인터변수의주소출력

---
+++ C/포인터_포인터변수의주소출력/포인터_포인터변수의주소출력.c
@@ -0,0 +1,20 @@
+/*
+	변수(일반, 포인터)의 주소 출력 : &
+*/
+#include <stdio.h>
+
+void main(void)
+{
+	int i;
+	int* p;
+
+	i = 100;
+	p = &i;
+
+	printf("일반변수 i의 값 : %d\n", i); // 100
+	printf("포인터변수 p가 참조하고 있는 변수의 값 : %d\n", *p); // 100
+
+	printf("일반변수 i의 주소값 : %u\n", &i); // %u : unsigned : 부호없는 정수
+	printf("포인터변수 p의 주소값 : %u\n", &p); // 
+	printf("포인터변수 p의 값 : %u\n", p); // i변수의 주소값과 동일
+}
