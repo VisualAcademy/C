64399d8b1018c7a8420e8c9d18c2b624b43d86a8
RedPlus <redplus@redplus.net>
2021-02-04 09:11:00 +0900

    문자열입출력함수

---
+++ C/문자열입출력함수/문자열입출력함수.c
@@ -0,0 +1,17 @@
+// 문자열 입출력함수
+// gets() : 문자열 입력 + 엔터, scanf()
+// puts() : 문자열 출력, printf()
+#include <stdio.h>
+
+void main(void)
+{
+	char strBuffer[100]; // 100자의 문자를 받을 수 있는 공간 마련
+
+	printf("최대 100자의 문자열을 입력하시오...\n");
+	gets(strBuffer);
+	puts(strBuffer);
+
+	printf("최대 100자의 문자열을 입력하시오...\n");
+	gets(strBuffer);
+	puts(strBuffer);
+}
