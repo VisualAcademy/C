7d8a821f78adfacfd6564571e32880884c83bab6
RedPlus <redplus@redplus.net>
2021-02-20 04:46:18 +0900

    Code Cleanup

--- C/ScanfArithmetic/ScanfArithmetic.c
+++ C/ScanfArithmetic/ScanfArithmetic.c
@@ -1,12 +1,10 @@
 // 2개의 숫자를 콘솔로부터 입력받은 후 산술 연산 값 출력하기
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 
-int main()
+main()
 {
-	int x, y;
-	int r;
+	int x, y, r;
 
 	printf("2개의 정수를 입력하세요: ");
 	r = scanf("%d %d", &x, &y);
@@ -16,6 +14,4 @@ int main()
 	printf("%d * %d = %d\n", x, y, (x * y));
 	printf("%d / %d = %d\n", x, y, (x / y));
 	printf("%d %% %d = %d\n", x, y, (x % y));
-
-	return 0;
 }
