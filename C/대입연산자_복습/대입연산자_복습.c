5f9018894bbe17efe780fda667cf6516930916c0
RedPlus <redplus@redplus.net>
2021-02-19 13:57:22 +0900

    대입연산자_복습

---
+++ C/대입연산자_복습/대입연산자_복습.c
@@ -0,0 +1,30 @@
+/*
+대입연산자 : =, +=, -=, *=, /=, %=
+*/
+#include <stdio.h>
+
+void main(void)
+{
+	int a, b, c;
+
+	a = 10; // a에 10을 대입
+	printf("%d\n", a);//10
+
+	a = 10 + 5; // a에 10 + 5를 대입
+	printf("%d\n", a);//15
+
+	a = a + 5; // 15 + 5
+	printf("%d\n", a);//20
+
+	a += 5; // a = a + 5와 동일한 코드를 줄임
+	printf("%d\n", a);//25
+
+					  //b = 2; a = b;
+	a = b = 2; // 나열 
+	printf("%d\n", a);
+
+	c = (b = 3, a = 8, a * b);
+	printf("%d \n", c);
+
+	system("pause");
+}
