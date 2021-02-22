7a8defec6a2df1986be2ac53d57ecf4c94738949
RedPlus <redplus@redplus.net>
2021-02-23 05:54:39 +0900

    IfNested

---
+++ C/IfNested/IfNested.c
@@ -0,0 +1,16 @@
+// ÁßÃ¸ µÈ if ¹®
+#include <stdio.h>
+
+main()
+{
+	char name = 'C';
+	int version = 18;
+
+	if (name == 'C')
+	{
+		if (version == 18)
+		{
+			printf("%c %d\n", name, version);
+		}
+	}
+}
