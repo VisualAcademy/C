d51d2c9c7f9aab4086f29ba3c308553b3b1c48f4
RedPlus <redplus@redplus.net>
2021-02-24 10:28:38 +0900

    일차원배열

---
+++ C/일차원배열/일차원배열.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(void)
+{
+    //[1] 배열 선언
+    int b[3]; // 배열
+    int i = 0;// 변수
+    //[2] 배열 초기화
+    b[0] = 10;
+    b[1] = 20;
+    b[2] = 30;
+    //[2] 변수 참조
+    printf("%d\n", b[0]);
+    printf("%d\n", b[1]);
+    printf("%d\n", b[2]);
+    //[3] 반복문을 사용해서 출력
+    for (i = 0; i < 3; i++)
+    {
+        printf("%d\n", b[i]);
+    }
+}
