9736e730020f314ac2cfade763727e6dc9a24916
RedPlus <redplus@redplus.net>
2021-04-13 11:35:09 +0900

    Code Cleanup

--- C/ForTriangle/ForTriangle.c
+++ C/ForTriangle/ForTriangle.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-main() {
-	int i, j;
+main() 
+{
+    int i, j;
 
-	for (i = 1; i <= 5; i++)
-	{
-		for (j = 1; j <= i; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
