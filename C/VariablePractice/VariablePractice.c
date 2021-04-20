4e858aaa0d5f3d3fb44e5fcbd31e648e95fe8b44
RedPlus <redplus@redplus.net>
2021-04-20 10:12:21 +0900

    VariablePractice

---
+++ C/VariablePractice/VariablePractice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // 여러 개의 변수를 선언과 동시에 초기화
+    int first = 10, second = 20, third = 30;
+
+    printf("%d, %d, %d\n", first, second, third);
+
+    system("pause");
+
+    return 0;
+}
