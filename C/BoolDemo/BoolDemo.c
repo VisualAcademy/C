67236c27c75f8c2aaac0861462a669c28718220d
RedPlus <redplus@redplus.net>
2021-04-24 02:04:34 +0900

    BoolDemo

---
+++ C/BoolDemo/BoolDemo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main()
+{
+	bool bln1;
+	bool bln2;
+
+	bln1 = true;	// Âü(1)
+	bln2 = false;	// °ÅÁş(0)
+
+	printf("%d %d\n", bln1, bln2);
+
+	getchar();
+}
