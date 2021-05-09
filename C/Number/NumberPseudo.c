3d99132160cda22c21221a5bb080a59f7d4c9fa6
WebCampKR <webcamp@outlook.kr>
2021-05-09 12:36:48 +0900

    NumberPseudo

---
+++ C/Number/NumberPseudo.c
@@ -0,0 +1,11 @@
+// 숫자 데이터 형식: int, float 등의 키워드로 숫자 데이터 저장
+#include <stdio.h>
+
+main()
+{
+    int i = 1234; // 정수 형식
+    float f = 3.14f; // 실수 형식
+
+    printf("%d\n", i);
+    printf("%f\n", f);
+}
