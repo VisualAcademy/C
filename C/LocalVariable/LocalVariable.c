05b5fae03020bf6a0f5a9b08841263fde79a54b6
RedPlus <redplus@redplus.net>
2021-05-11 21:20:13 +0900

    지역변수

---
+++ C/LocalVariable/LocalVariable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i;
+	int j;
+	int k;
+
+	i = 1234;
+	j = 2345;
+	k = 3456;
+
+	printf("%d\n", i + j + k); // 7035
+
+	system("pause");
+
+	return 0;
+}
