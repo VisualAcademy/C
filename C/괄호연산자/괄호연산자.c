32ff9213460c88a4f386f3ce923e5baab7060089
RedPlus <redplus@redplus.net>
2021-05-21 01:45:22 +0900

    괄호연산자

---
+++ C/괄호연산자/괄호연산자.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+	float a, b, c, d, e;
+
+	a = 2.5 * 5 / 2.0;
+	b = 2.5 * (5 / 2.0);	// () : 괄호 연산자 : 우선순위 변경
+	c = 5 / 2;				// 5 / 2 = 2
+	d = 5 / (float)2;		// () : 변환(캐스팅) 연산자
+	e = 5 / 2.0;
+
+	printf("2.5 * 5 / 2 = %f\n", a);
+	printf("2.5 * (5 / 2) = %f\n", b);
+	printf(" %f \n", c); // 2
+	printf(" %f \n", d); // 2.5
+	printf(" %f \n", e); // 2.5
+
+	system("pause");
+}
