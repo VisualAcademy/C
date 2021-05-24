ded5eddb5d477f317d2a752bf2bde625aab4c224
RedPlus <redplus@redplus.net>
2021-05-24 09:32:57 +0900

    IfDemo.c

---
+++ C/13_IfElse/IfDemo.c
@@ -0,0 +1,17 @@
+// if 문을 비교 연산자와 함께 사용하기
+#include <stdio.h>
+
+main()
+{
+    int x = 10;
+
+	if (x == 10)
+	{
+		printf("x는 %d입니다.\n", x);
+	}
+
+	if (x != 20)
+	{
+		printf("x는 20이 아닙니다.\n");
+	}
+}
