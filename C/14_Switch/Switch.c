d097f97c5763b8a267cf5ea2017fb424e91b6d6a
RedPlus <redplus@redplus.net>
2021-05-25 20:26:49 +0900

    Switch

---
+++ C/14_Switch/Switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+main()
+{
+    int answer = 0;
+    int r = 0;
+    printf("정수를 입력하세요.\n");
+    r = scanf_s("%d", &answer); r = getchar();
+
+    // 선택문
+    switch (answer)
+    {
+        case 1:
+            printf("1을 선택했습니다.\n");
+            break;
+        case 2:
+            printf("2을 선택했습니다.\n");
+            break;
+        case 3:
+            printf("3을 선택했습니다.\n");
+            break;
+        default:
+            printf("그냥 찍으셨군요.\n");
+            break;
+    }
+}
