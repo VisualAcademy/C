ad1b81ebfd67b7a7b44386389cfc5f23734ac709
RedPlus <redplus@redplus.net>
2021-05-25 10:03:48 +0900

    SwitchExpression

---
+++ C/14_Switch/SwitchExpression.c
@@ -0,0 +1,20 @@
+// switch 문 사용하기
+#include <stdio.h>
+
+main()
+{
+    int x = 2;
+
+    switch (x)
+    {
+        case 1:
+            printf("1입니다.\n"); // x가 1일 때 실행
+            break;
+        case 2:
+            printf("2입니다.\n"); // x가 2일 때 실행
+            break;
+        default:
+            printf("1과 2가 아닙니다.\n");
+            break;
+    }
+}
