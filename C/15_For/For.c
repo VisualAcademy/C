442414bf3adb2c3325b97f7b28b0eaf4617cf3a5
RedPlus <redplus@redplus.net>
2021-05-31 05:16:17 +0900

    Code Cleanup

--- C/15_For/For.c
+++ C/15_For/For.c
@@ -3,11 +3,11 @@
 
 main()
 {
-	int i = 0;
+    int i = 0;
 
-	// 1부터 5까지 1씩 증가 => 5번 반복
-	for (i = 1; i <= 5; i++)
-	{
-		printf("Count: %d\n", i);
-	}
+    // 1부터 5까지 1씩 증가 => 5번 반복
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Count: %d\n", i);
+    }
 }
