38fe8cb4b7e77bd4b2571b23a6b05f00af04f452
RedPlus <redplus@redplus.net>
2021-06-02 00:29:00 +0900

    매크로_내장매크로

---
+++ C/99_Temp/매크로_내장매크로.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(void)
+{
+	printf("현재 파일명 : %s\n", __FILE__);
+	printf("현재 컴파일 날짜 : %s\n", __DATE__);
+	printf("현재 컴파일 시간 : %s\n", __TIME__);
+	printf("현재 코드의 라인번호 : %d\n", __LINE__);//8
+}
