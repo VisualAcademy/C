b0066e7d54de5491bcfc4a54e4980e9557a44598
RedPlus <redplus@redplus.net>
2021-06-24 13:18:42 +0900

    Code Cleanup

---
+++ C/프로그래밍언어비교/프로그래밍언어비교.cpp
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//#include <iostream.h>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void main()
+{
+	//printf("¾È³ç\n");
+	//cout << "¾È³ç" << endl;
+	char s[100];
+	std::cin >> s;
+	std::cout << "¾È³ç" << std::endl;
+	std::cout << "¹æ°¡" << std::endl;
+	std::cout << s << std::endl;
+	cout << "»ý·«°¡´É" << endl;
+}
