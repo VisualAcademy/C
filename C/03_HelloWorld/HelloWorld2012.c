6b42a9a48218c3d4910f50ffd4fd49db5dfb581b
RedPlus <redplus@redplus.net>
2021-06-25 04:39:14 +0900

    Code Cleanup

---
+++ C/03_HelloWorld/HelloWorld2012.c
@@ -0,0 +1,11 @@
+/*
+처음으로 작성해 보는 C언어 프로그램
+*/
+#include <stdio.h>
+
+void main(void)
+{
+	printf("안녕하세요. C언어!!!\n");
+
+	system("pause");
+}
--- C/Start/Start.c
+++ C/Start/Start.c
@@ -5,3 +5,15 @@ main()
 {
 	printf("안녕하세요.");
 }
+
+///*
+//	타이핑 및 결과 확인 그리고 이해하도록 노력
+//*/
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//main()
+//{
+//	printf("Hello World\n");
+//	system("pause");
+//}
