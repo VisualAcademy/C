0af1afb35fb4e314292f14b46cff2e92aab560d5
RedPlus <redplus@redplus.net>
2021-07-07 19:39:29 +0900

    Code Cleanup

---
+++ C/데이터구조/데이터구조_이진탐색트리(리스트).c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+struct tnode
+{
+	struct tnode* left;
+	char name[25];
+	struct tnode* right;
+};
+
+struct tnode* talloc(void);
+
+void main(void)
+{
+	char dat[25];
+	struct tnode* root, * p, * old;
+
+	root = talloc();
+	scanf("%s", root->name);
+	root->left = root->right = NULL; // 루트 초기화
+
+	while (scanf("%s", dat) != EOF)
+	{
+		p = root;
+		while (p != NULL)
+		{
+			old = p;
+			if (strcmp(dat, p->name) <= 0)
+			{
+				p = p->left;
+			}
+			else
+			{
+				p = p->right;
+			}
+
+			p = talloc();
+			strcpy(p->name, dat);
+			p->left = p->right = NULL;
+			if (strcmp(dat, old->name) <= 0)
+			{
+				old->left = p;
+			}
+			else
+			{
+				old->right = p;
+			}
+		}
+	}
+}
+
+struct tnode* talloc(void)
+{
+	return (struct tnode*)malloc(sizeof(struct tnode));
+}
