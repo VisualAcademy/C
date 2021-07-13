c2bff569978b497313ab51482aeb8c5e39efa18b
RedPlus <redplus@redplus.net>
2021-07-14 01:05:31 +0900

    OperatorDescription

---
+++ C/09_Operator/OperatorDescription.c
@@ -0,0 +1,13 @@
+//[?] 연산자(Operator): 더하기, 빼기와 같은 연산을 진행하는 키워드
+#include <stdio.h>
+
+main()
+{
+    //[?] 식(Expression)
+    printf("%d\n", 3 + 5); // 8
+    printf("%d\n", 3 - 5); // -2
+
+    //[?] 문(Statement) 
+    printf("%d\n", 3 * 5); // 15
+    printf("%d\n", 3 / 5); // 0
+}
