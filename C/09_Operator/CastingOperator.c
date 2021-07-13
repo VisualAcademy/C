a776a169a09c0663cdb3d4a18448851656174878
RedPlus <redplus@redplus.net>
2021-07-14 01:14:20 +0900

    Code Cleanup

--- C/09_Operator/CastingOperator.c
+++ C/09_Operator/CastingOperator.c
@@ -4,4 +4,10 @@ main()
 {
     int number = (int)3.14; // 3.14를 정수(int)로 변환
     printf("%d\n", number); // 3
+
+    //long number = (long)3.141592;
+    //printf("%d\n", number); // 3
+
+    //double number = (double)3.141592;
+    //printf("%lf\n", number); // 3.141592
 }
