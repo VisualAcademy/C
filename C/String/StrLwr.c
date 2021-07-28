c5efa5815239434906e3076fdc592df1d4c5106b
RedPlus <redplus@redplus.net>
2021-07-28 15:55:13 +0900

    _strlwr_s

---
+++ C/String/StrLwr.c
@@ -0,0 +1,16 @@
+//[?] _strlwr_s() 함수: 문자열을 모두 소문자로 변경 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[20] = "C Language";
+
+    printf("%s\n", str); // C Language
+
+    _strlwr_s(str, 20); 
+
+    printf("%s\n", str); // c language
+
+    return 0;
+}
