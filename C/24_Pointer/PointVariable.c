481ff645c54ab787be4c3328e4e00a2cbb944bbb
RedPlus <redplus@redplus.net>
2021-08-08 06:43:56 +0900

    Code Cleanup

--- C/24_Pointer/PointVariable.c
+++ C/24_Pointer/PointVariable.c
@@ -8,6 +8,6 @@ main()
     //[2] 포인터 변수 선언과 동시에 num 변수를 ptr 변수로 참조(Reference)
     int* ptr = &num; // & 기호로 num 변수의 주솟값을 읽어 포인터 변수에 저장
 
-    //[3] ptr 변수로 참조된 값 출력
+    //[3] ptr 변수로 참조된 값 출력: 역참조(Dereference)
     printf("%d\n", *ptr); // * 기호로 ptr 변수가 참조하고 있는 곳의 값을 출력
 }
