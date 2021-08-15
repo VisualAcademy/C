ec392c03896d356a986b6dd2c051ddebb3f1b117
RedPlus <redplus@redplus.net>
2021-08-15 09:30:42 +0900

    Code Cleanup

---
+++ C/String/StringCopy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char messages[6];
+
+    //[?] C 언어는 문자 배열에 문자 리터럴을 사용해서 초기화 불가능
+    //messages = "C/C++";
+
+    //[!] 문자 배열에 문자 리터럴을 초기화할 때에는 strcpy() 함수 사용
+    strcpy(messages, "C/C++");
+
+    puts(messages);
+
+    return 0;
+}
