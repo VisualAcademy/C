2600267a848bf1eed70e247c21397611306b56be
RedPlus <redplus@redplus.net>
2021-08-17 11:32:05 +0900

    Code Cleanup

--- C/IncludeDemo/IncludeDemo.c
+++ C/IncludeDemo/IncludeDemo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+// max.h 파일로 이동
 //int max(int first, int second);
+
 #include "max.h"
 
 int main()
@@ -10,6 +12,7 @@ int main()
     return 0;
 }
 
+// max.c 파일로 이동
 //int max(int first, int second)
 //{
 //    return first > second ? first : second;
