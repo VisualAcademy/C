731b5be51ce739f6da8709db4dc4604f2a2936d2
RedPlus <redplus@redplus.net>
2021-08-25 07:13:44 +0900

    MacroFunction.c

---
+++ C/PreProcessor/MacroFunction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//[!] 매크로 함수 선언
+#define max(x, y) x > y ? x : y
+#define min(x, y) (x > y ? y : x)
+
+int main()
+{
+    printf("최댓값: %d\n", max(3, 5)); // 5
+    printf("최솟값: %d\n", min(-3, -5)); // -5
+
+    return 0;
+}
