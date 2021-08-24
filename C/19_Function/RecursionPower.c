efeee68ef8868cd8c2544afc7b9d2a3d7c337ca0
RedPlus <redplus@redplus.net>
2021-08-25 03:02:11 +0900

    RecursionPower

---
+++ C/19_Function/RecursionPower.c
@@ -0,0 +1,18 @@
+// 재귀(Recursion)를 사용한 2 ^ n 승 패턴 구하기
+#include <stdio.h>
+
+int MyPower(int num, int cnt)
+{
+    if (cnt == 0)
+    {
+        return 1;
+    }
+    return num * MyPower(num, --cnt); // 2 * (2 * (1))
+}
+
+int main()
+{
+    printf("%d\n", MyPower(2, 2)); // 2 * 2 * 1 = 4
+
+    return 0;
+}
