cf1c4de6c7d2d00547ce9c11435dbd2638cf5492
RedPlus <redplus@redplus.net>
2021-09-09 10:43:25 +0900

    Code Cleanup

--- C/18_Array/ArrayTwoFor.c
+++ C/18_Array/ArrayTwoFor.c
@@ -1,10 +1,10 @@
 // 2차원 배열을 2중 for 문으로 반복해서 출력하기 
 #include <stdio.h>
 
-main()
+int main(void)
 {
     int i, j;
-    int arr[2][3] = { 1, 2, 3, 4, 5, 6 };
+    int arr[2][3] = { 1, 2, 3, 4, 5, 6 }; // 안 쪽 중괄호 생략 가능
 
     for (i = 0; i < 2; i++)
     {
@@ -13,4 +13,6 @@ main()
             printf("arr[%d][%d] = %d\n", i, j, arr[i][j]);
         }
     }
+
+    return 0;
 }
