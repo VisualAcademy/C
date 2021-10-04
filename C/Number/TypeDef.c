d090cf714e84cafab9d3496f31f3ce235268f89f
Park YongJune <redplus@redplus.net>
2021-10-04 23:53:15 +0900

    Code Cleanup

--- C/Number/TypeDef.c
+++ C/Number/TypeDef.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
     typedef int number; // int 형식을 number 이름의 별칭으로 사용
 
-    number age = 123;
+    number age = 123; // int age = 123; 코드와 동일
 
     printf("%d\n", age);
 
