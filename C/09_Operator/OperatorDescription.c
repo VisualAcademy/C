583a74219ef0b825b3b77f95cfe3d9505bf663a3
Park YongJune <redplus@redplus.net>
2021-10-08 14:34:55 +0900

    Code Cleanup

--- C/09_Operator/OperatorDescription.c
+++ C/09_Operator/OperatorDescription.c
@@ -1,7 +1,7 @@
 //[?] 연산자(Operator): 더하기, 빼기와 같은 연산을 진행하는 키워드
 #include <stdio.h>
 
-main()
+int main(void)
 {
     //[?] 식(Expression)
     printf("%d\n", 3 + 5); // 8
@@ -10,4 +10,6 @@ main()
     //[?] 문(Statement) 
     printf("%d\n", 3 * 5); // 15
     printf("%d\n", 3 / 5); // 0
+
+    return 0; 
 }
