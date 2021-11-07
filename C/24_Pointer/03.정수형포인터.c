c080300b1ad0f34b90780f059d37f15837820591
Park YongJune <redplus@redplus.net>
2021-11-08 01:27:31 +0900

    Code Cleanup

--- C/24_Pointer/03.정수형포인터.c
+++ C/24_Pointer/03.정수형포인터.c
@@ -1,3 +1,4 @@
+// 03.정수형포인터.c
 #include <stdio.h>
 
 int main(void)
@@ -8,15 +9,15 @@ int main(void)
     int* pa = &a;
 
     printf("a : %d\n", a);//365
-    printf("&a : %u\n", &a);//1244992
+    printf("&a : %p\n", &a);//1244992
     //printf("*a : %d\n", *a);
 
     // pa에 들어있는 다른 변수(공간)의 번지 수
-    printf("pa : %d\n", pa);//1244992번지
+    printf("pa : %p\n", pa);//1244992번지
     // pa가 참조하고 있는 변수에 들어있는 값
     printf("*pa : %d\n", *pa);//365
     // pa 변수 자체의 고유 어드레스(주소)
-    printf("&pa : %u\n", &pa);//1244980
+    printf("&pa : %p\n", &pa);//1244980
 
     return 0;
 }
