d8efb1651a3d19d1bc23628461c0ce70808254ba
Park YongJune <redplus@redplus.net>
2021-11-26 07:10:08 +0900

    Code Cleanup

---
+++ C/13_IfElse/IfExam.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 0;
+    int r = scanf("%d", &age);
+    if (age < 14)
+    {
+        printf("만 14세 미만 어린이는 부모님의 동의가 필요합니다.\n");
+    }
+    return 0;
+}
--- C/13_IfElse/IfPractice.c
+++ C/13_IfElse/IfPractice.c
@@ -1,11 +1,12 @@
+// 음수 판별 
 #include <stdio.h>
 
 int main(void)
 {
-	int number = -1234;
-	if (number < 0)
-	{
-		printf("%d은(는) 음수입니다.\n", number);
-	}
-	return 0; 
+    int number = -1234;
+    if (number < 0)
+    {
+        printf("%d은(는) 음수입니다.\n", number);
+    }
+    return 0;
 }
