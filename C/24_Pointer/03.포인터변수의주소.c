dcbddbabef1c284bbadea800658c43eba162a607
Park YongJune <redplus@redplus.net>
2021-11-26 16:35:30 +0900

    03.포인터변수의주소.c

--- C/24_Pointer/03.포인터변수의주소.c
+++ C/24_Pointer/03.포인터변수의주소.c
@@ -1,15 +1,19 @@
-main()
+#include <stdio.h>
+
+int main(void)
 {
-	int a, * pa;
+    int a, * pa;
 
-	a = 10;
-	pa = &a;
+    a = 10;
+    pa = &a;
 
-	//[1] 값형 a변수
-	printf("%d\n", a);//a의 값
-	printf("%d\n", &a);//a의 메모리상의 주소 : 1244992
-	//[2] 포인터형 pa변수
-	printf("%d\n", pa);//pa변수의 실제값:a의주소값 : 1244992
-	printf("%d\n", &pa);//pa변수의 주소:1244980
-	printf("%d\n", *pa);//pa변수가참조하는곳의실제데이터
+    //[1] 값형 a변수
+    printf("%d\n", a);//a의 값
+    printf("%d\n", &a);//a의 메모리상의 주소 : 15727580
+    //[2] 포인터형 pa변수
+    printf("%d\n", pa);//pa변수의 실제값:a의주소값 : 15727580
+    printf("%d\n", &pa);//pa변수의 주소:15727568
+    printf("%d\n", *pa);//pa변수가참조하는곳의실제데이터
+
+    return 0;
 }
