a186e2875439f6e22b0e3cfe00da0522ae4b0858
Park YongJune <redplus@redplus.net>
2021-12-22 18:08:10 +0900

    .

--- C/13_IfElse/IfExam.c
+++ C/13_IfElse/IfExam.c
@@ -4,10 +4,12 @@
 int main(void)
 {
     int age = 0;
-    int r = scanf("%d", &age);
+    int _ = scanf("%d", &age);
+
     if (age < 14)
     {
         printf("만 14세 미만 어린이는 부모님의 동의가 필요합니다.\n");
     }
+
     return 0;
 }
