b8205aae5845e2c76d73a14b8fd51443490d6185
Park YongJune <redplus@redplus.net>
2021-12-26 10:17:45 +0900

    .

--- C/19_Function/GetSumTwoNumber.c
+++ C/19_Function/GetSumTwoNumber.c
@@ -1,16 +1,18 @@
+// get_sum_two_number.c
+// C 언어 코드 샘플 - 두 수의 합을 구하는 함수 
 #include <stdio.h>
 
 // 두 수의 합을 구하는 함수
-double GetSum(double x, double y)
+double get_sum(double x, double y)
 {
     double r = x + y;
     return r;
 }
 
-int main()
+int main(void)
 {
-    double result = GetSum(3.0, 0.14);
-    printf("%.2lf\n", result); // 3.14
+    double result = get_sum(3.0, 0.14);
+    printf("%lf\n", result); // 3.14
 
     return 0;
 }
