0c7d043481f3118f13d6521b47ca925c6a76d061
Park YongJune <redplus@redplus.net>
2022-01-05 00:30:27 +0900

    .

--- C/09_Operator/Remainder.c
+++ C/09_Operator/Remainder.c
@@ -1,7 +1,7 @@
 //[?] 나머지 연산자(Remainder Operator): 몫이 아닌 나머지 값 구하기
 #include <stdio.h>
 
-main()
+int main(void)
 {
     int first = 5;
     int second = 3;
@@ -10,4 +10,6 @@ main()
     result = first % second; // 5 / 3 => 나머지 값인 2를 반환
 
     printf("나머지 값: %d\n", result); // 몫: 1, 나머지: {2}
+
+    return 0;
 }
