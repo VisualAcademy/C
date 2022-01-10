29629ed3266dcb621501a837d10969569a65cb8a
Park YongJune <redplus@redplus.net>
2022-01-11 04:47:32 +0900

    .

--- C/GetChar/GetChar.c
+++ C/GetChar/GetChar.c
@@ -1,13 +1,15 @@
+// getchar_demo.c
 // getchar() 함수: 키보드로부터 하나의 문자 입력
 #include <stdio.h> // getchar()
 
 int main()
 {
-    char bloodType;
+    char blood_type;
 
     printf("당신의 혈액형은? _\b");
-    bloodType = getchar();
-    printf("%c형입니다.\n", bloodType);
+    blood_type = getchar();
+
+    printf("%c형입니다.\n", blood_type);
 
     return 0;
 }
