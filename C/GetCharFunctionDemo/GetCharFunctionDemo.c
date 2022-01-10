e4123220abe87f966c834b54d37d58f58235b9d7
Park YongJune <redplus@redplus.net>
2022-01-11 04:35:08 +0900

    Code Cleanup

--- C/GetCharFunctionDemo/GetCharFunctionDemo.c
+++ C/GetCharFunctionDemo/GetCharFunctionDemo.c
@@ -1,9 +1,14 @@
+// getchar_function.c
 #include <stdio.h>
 
-main()
+int main(void)
 {
     int c;
+
     printf("문자 하나 입력: _\b");
     c = getchar();
+
     printf("입력한 문자: %c\n", c);
+
+    return 0;
 }
