e6cd8e3bc463c72ea40ab5308fccd86b242bac22
Park YongJune <redplus@redplus.net>
2022-08-12 01:23:23 +0900

    구조체_구조체비트필드.c

--- C/구조체비트필드/구조체_구조체비트필드.c
+++ C/구조체비트필드/구조체_구조체비트필드.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+
 // 멤버(필드)의 데이터 저장 범위
-// i : 0~255
-// j : 0~3
-struct A {
+// i: 0~255
+// j: 0~3
+struct A 
+{
 	int i;
 	int j;
 };
+
 // 구조체 비트필드를 적용한 구조체.
-struct B {
+struct B 
+{
 	int i : 8;//2^8=256
 	int j : 4;//2^4=8
 };
-void main() {
+
+void main() 
+{
 	struct A a; struct B b;
 	a.i = 100; a.j = 3; b.i = 100; b.j = 3;
 	printf("%d %d\n", a.i, a.j);
