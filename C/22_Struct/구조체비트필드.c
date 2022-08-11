8a887482d0815cf3f45de922296bb15b41895c48
Park YongJune <redplus@redplus.net>
2022-08-12 01:19:51 +0900

    구조체비트필드.c

---
+++ C/22_Struct/구조체비트필드.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct A
+{
+	int a : 8; // 0~255 : 2^8 = 256
+	int b : 4; // 0~3 : 2^2 = 4
+};
+
+int main(void)
+{
+	struct A a;
+
+	a.a = 100;
+	a.b = 3;
+
+	printf("%d %d \n", a.a, a.b);
+	printf("Size : %d\n", sizeof(a));//8Byte : 64Bit
+
+	system("pause");
+}
