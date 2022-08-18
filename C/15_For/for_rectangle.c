728ec09a822ebc6ad290e60c748ecac542515c58
Park YongJune <redplus@redplus.net>
2022-08-18 17:36:44 +0900

    for_nested_triangle

--- C/15_For/for_rectangle.c
+++ C/15_For/for_rectangle.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    // for 중첩 루프로 (3 * 3)번 반복하여 정사각형 모양 출력
+    // for 중첩 루프로 (3 * 5)번 반복하여 직사각형 모양 출력
     for (int i = 0; i < 3; i++) // 높이(height) 반복
     {
-        for (int j = 0; j < 3; j++) // 너비(width) 반복
+        for (int j = 0; j < 5; j++) // 너비(width) 반복
         {
             printf("*"); // 별표 출력
         }
