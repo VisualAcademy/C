0bb10bdfcd0667886e3d2d05eee245bfd42f2fc9
Park YongJune <redplus@redplus.net>
2022-10-07 00:41:46 +0900

    .

---
+++ C/24_Pointer/포인터_변수의주소출력.c
@@ -0,0 +1,15 @@
+/*
+	&연산자와 %#x : 변수의 주소(번지) 출력
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 10;
+	int j = 20;
+	int k = 30;
+
+	printf("%d, %x, %#x\n", i, i, &i); // %d : 10진수
+	printf("%d, %x, %#x\n", j, j, &j); // %x : 16진수
+	printf("%d, %x, %#x\n", k, k, &k); // %#x : 변수의 주소(번지;Address) 출력
+}
