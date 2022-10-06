2b47bd1f186d3c8841f236f3a851c2cc916c6255
Park YongJune <redplus@redplus.net>
2022-10-07 01:48:36 +0900

    .

--- C/알고리즘_포인터_최대값/알고리즘_포인터_최대값.c
+++ C/알고리즘_포인터_최대값/알고리즘_포인터_최대값.c
@@ -2,21 +2,25 @@
 #include <stdio.h>
 #include <limits.h> // INT_MIN, INT_MAX
 
-void main()
+void main(void)
 {
-	//[1] Input
-	int intNum[] = { -3, -2, -1, -4, -5 };
-	int intMax = INT_MIN;
-	int i;
-	int* p; p = intNum;//&intNum[0];
-	//[2] Process : MAX
-	for (i = 0; i < 5; i++)
-	{
-		if (intMax < *(p + i))
-		{
-			intMax = *(p + i);
-		}
-	}
-	//[3] Output
-	printf("최대값 : %d\n", intMax);
+    //[1] Input
+    int intNum[] = { -3, -2, -1, -4, -5 };
+    int intMax = INT_MIN;
+    int i;
+    int* p; p = intNum; // &intNum[0];
+
+    //[2] Process: MAX
+    for (i = 0; i < 5; i++)
+    {
+        if (intMax < *(p + i))
+        {
+            intMax = *(p + i);
+        }
+    }
+
+    //[3] Output
+    printf("최대값: %d\n", intMax);
+
+    return 0;
 }
