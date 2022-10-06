a4cafd53fa6afeac8a433ee75423cb48630c19df
Park YongJune <redplus@redplus.net>
2022-10-07 01:04:39 +0900

    .

--- C/24_Pointer/포인터배열_배열의배열.c
+++ C/24_Pointer/포인터배열_배열의배열.c
@@ -4,13 +4,12 @@
 int main(void)
 {
     char* arr[3]; // 기본: 문자 3개 저장, 포인터 배열: 문자열 3개를 저장해 놓을 그릇
-    int i;
 
     arr[0] = "C언어";
     arr[1] = "C++";
     arr[2] = "C#";
 
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         printf("%s \n", arr[i]);
     }
