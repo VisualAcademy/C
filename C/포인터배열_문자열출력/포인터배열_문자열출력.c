b9682a62886f10bebfa86afd9b0d01a980d19184
Park YongJune <redplus@redplus.net>
2022-10-07 00:53:41 +0900

    .

--- C/포인터배열_문자열출력/포인터배열_문자열출력.c
+++ C/포인터배열_문자열출력/포인터배열_문자열출력.c
@@ -3,16 +3,18 @@
 
 int main(void)
 {
-	// C언어 : 문자열 처리 키워드 X : Java/C# : String 키워드
-	//string s = "안녕하세요...";
-	char* str;
+    // C언어: 문자열 처리 키워드: X, Java/C#: String 키워드
+    //string s = "안녕하세요...";
+    char* str;
 
-	str = "안녕하세요. 반갑습니다."; // 한글/일어/중국어 : 2byte코드, 영어 : 1byte코드
+    str = "안녕하세요. 반갑습니다."; // 한글/일어/중국어: 2byte코드, 영어: 1byte코드
 
-	printf("%s\n", str);		// 전체 출력
-	printf("%s\n", str + 12);	// 반갑습니다.
-	printf("%s\n", str + 3);	// '녕'을 반쪽만 출력하다보니 깨져서 보여짐
+    printf("%s\n", str);		// 전체 출력
+    printf("%s\n", str + 12);	// 반갑습니다.
+    printf("%s\n", str + 3);	// '녕'을 반쪽만 출력하다보니 깨져서 보여짐
 
-	str = "상수에 값을 덮어쓰기"; // 허용, scanf() 접근 불가
-	printf("%s\n", str);		// 전체 출력
+    str = "상수에 값을 덮어쓰기"; // 허용, scanf() 접근 불가
+    printf("%s\n", str);		// 전체 출력
+
+    return 0; 
 }
