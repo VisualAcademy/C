2ad9324107f69e2789addf4a83d43479bf916d71
Park YongJune <redplus@redplus.net>
2022-10-07 00:06:31 +0900

    .

--- C/포인터배열_이차원배열참조/포인터배열_이차원배열참조.c
+++ C/포인터배열_이차원배열참조/포인터배열_이차원배열참조.c
@@ -1,18 +1,18 @@
 /*
-	포인터변수에 2차원배열을 참조해서 호출
+    포인터변수에 2차원배열을 참조해서 호출
 */
 #include <stdio.h>
 
 int main(void)
 {
-	int arr[2][3] = { {1, 2, 3}, {4, 5, 6} };
-	int* p;
-	int i;
+    int arr[2][3] = { {1, 2, 3}, {4, 5, 6} };
+    int* p;
+    int i;
 
-	p = arr;
+    p = arr;
 
-	for (i = 0; i < sizeof(arr) / sizeof(int); i++)
-	{
-		printf("%d \n", *(p + i));
-	}
+    for (i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        printf("%d \n", *(p + i));
+    }
 }
