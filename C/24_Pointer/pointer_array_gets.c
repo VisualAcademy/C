f7e548d6b0320e4fad3f54447b23b988d735c5d1
Park YongJune <redplus@redplus.net>
2022-10-08 12:09:46 +0900

    .

---
+++ C/24_Pointer/pointer_array_gets.c
@@ -0,0 +1,15 @@
+// 화면으로부터 문자열을 입력받아서 출력하는 프로그램: scanf() 대신에 gets() 함수 사용
+#include <stdio.h>
+
+int main(void)
+{
+	char str[100];
+
+	printf("문자열을 입력하시오 : ");
+	//scanf("%s", str);
+	gets(str); // 한줄의 문자열을 읽어오는 함수
+
+	printf("%s\n", str);
+
+	return 0;
+}
---
+++ C/24_Pointer/pointer_array_string.c
@@ -0,0 +1,18 @@
+/*
+	포인터를 사용하여 문자배열(문자열)을 저장하기
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	char* p;
+
+	c = 'A';
+	p = "Hello World"; // "Hello World\0"
+
+	printf("%c\n", c); // A
+	printf("%s\n", p); // Hello World
+
+	return 0;
+}
