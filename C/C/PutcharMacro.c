8c358b18a74cdceffff8c930fc1e7b2e26362f88
Park YongJune <redplus@redplus.net>
2022-11-17 02:59:59 +0900

    putchar_macro

--- C/C/PutcharMacro.c
+++ C/C/PutcharMacro.c
@@ -1,7 +1,7 @@
-//[?] putchar() 함수: 표준 출력(stdout)에 문자 하나를 출력
+//[?] putchar() 매크로: 표준 출력(stdout)에 문자 하나를 출력
 #include <stdio.h>
 
-int main()
+int main(void)
 {
     putchar('Z');
     putchar('\n');
---
+++ C/C/putchar_macro.c
@@ -0,0 +1,11 @@
+//[?] putchar() 매크로: 표준 출력(stdout)에 문자 하나를 출력
+#include <stdio.h>
+
+int main(void)
+{
+    putchar('Z'); // Z 출력
+    putchar('\n'); // 줄 바꿈
+    putchar(90); // Z 출력
+
+    return 0;
+}
