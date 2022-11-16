52de87de9feb0e727778f825487f18af7b06a79e
Park YongJune <redplus@redplus.net>
2022-11-17 04:27:51 +0900

    .

--- C/C/getchar_macro.c
+++ C/C/getchar_macro.c
@@ -6,7 +6,7 @@ int main(void)
     int anykey;
 
     printf("키보드에서 아무키나 누르고 엔터치세요.\n");
-    anykey = getchar();
+    anykey = getchar(); // 입력 대기
     printf("입력한 값은 %c입니다.\n", anykey);
 
     return 0;
