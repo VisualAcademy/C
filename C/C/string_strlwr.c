26d9a1d74ced9b9d75c788072d7b4604ad926e35
Park YongJune <redplus@redplus.net>
2023-02-11 16:25:32 +0900

    string_strlwr.c

---
+++ C/C/string_strlwr.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[80];
+
+    printf("문자열을 입력하세요: ");
+    scanf("%s", str);
+
+    _strlwr(str);
+    printf("소문자로 변환된 문자열: %s\n", str);
+
+    return 0;
+}
