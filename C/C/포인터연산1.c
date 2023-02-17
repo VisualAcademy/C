31fe3ed47e3839b35e58c158c06dcbc0b5c45664
Park YongJune <redplus@redplus.net>
2023-02-17 15:04:26 +0900

    .

--- C/C/포인터연산1.c
+++ C/C/포인터연산1.c
@@ -9,16 +9,16 @@ int main(void)
     int* pnum = num; // pnum은 num의 첫 번째 원소를 가리킨다.
 
     //*pnum++ : *(pnum++)
-    printf("*pnum++ : %d \n", *pnum++); // 0, pnum이 가리키는 원소 출력, pnum을 1 증가
+    printf("*pnum++ : %d \n", *pnum++); // 1, pnum이 가리키는 원소 출력, pnum을 1 증가
 
     //*pnum-- : *(pnum--)
-    printf("*pnum-- : %d \n", *pnum--); // 1, pnum이 가리키는 원소를 출력하고, pnum을 1 감소시킨다.
+    printf("*pnum-- : %d \n", *pnum--); // 2, pnum이 가리키는 원소를 출력하고, pnum을 1 감소시킨다.
 
     //*++pnum : *(++pnum)
-    printf("*++pnum : %d \n", *++pnum); // 1, pnum을 1 증가시키고, pnum이 가리키는 원소를 출력한다.
+    printf("*++pnum : %d \n", *++pnum); // 2, pnum을 1 증가시키고, pnum이 가리키는 원소를 출력한다.
 
     //*--pnum : *(--pnum)
-    printf("*--pnum : %d \n", *--pnum); // 0, pnum을 1 감소시키고, pnum이 가리키는 원소를 출력한다.
+    printf("*--pnum : %d \n", *--pnum); // 1, pnum을 1 감소시키고, pnum이 가리키는 원소를 출력한다.
 
     return 0;
 }
