a6f2e64a9d02a5d71eb3dbe2eb72325c948321a0
Park YongJune <redplus@redplus.net>
2023-02-23 22:49:39 +0900

    .

--- C/C/pointer_string_array.c
+++ C/C/pointer_string_array.c
@@ -3,16 +3,13 @@
 
 int main(void)
 {
-    // 문자 배열 선언과 초기화
-    char a[] = { 'a', 'b', 'c', 'd', 'e' };
-
     // 문자열 포인터 배열 선언과 초기화
-    const char* b[] = { "aaa", "bbb", "ccc", "ddd", "eee" };
+    const char* strs[] = { "aaa", "bbb", "ccc", "ddd", "eee" };
 
     // b 배열의 요소들을 출력하는 for 루프
     for (int i = 0; i < 5; i++)
     {
-        printf("%s\n", b[i]);
+        printf("%s\n", strs[i]);
     }
 
     return 0;
