ad258f8de5550c307c05cb111ecac63ffd6c59b7
Park YongJune <redplus@redplus.net>
2023-03-09 23:40:18 +0900

    .

--- C/C/call_by_value_and_reference.c
+++ C/C/call_by_value_and_reference.c
@@ -1,23 +1,21 @@
 // call_by_value_and_reference.c
-
-// 값형/값전달/ValueType/Call By Value
-// 참조형/참조(주소)전달/ReferenceType/Call By Reference
-// 함수에서 일반변수와 포인터변수 사용
-
 #include <stdio.h>
 
 // 값 전달(Call By Value)을 이용하여 변수의 값을 12 증가시키는 함수
-void increase_by_12(int value) {
+void increase_by_12(int value) 
+{
     value += 12;
 }
 
 // 참조 전달(Call By Reference)을 이용하여 변수의 값을 15 증가시키는 함수
-void increase_by_15(int* value) {
+void increase_by_15(int* value) 
+{
     *value += 15;
 }
 
 // Entry Point
-int main(void) {
+int main(void) 
+{
     int num = 7;
 
     // 값을 전달하여 num 값을 증가시키는 함수 호출
@@ -36,3 +34,7 @@ int main(void) {
 
     return 0;
 }
+
+// 값형/값전달/ValueType/Call By Value
+// 참조형/참조(주소)전달/ReferenceType/Call By Reference
+// 함수에서 일반변수와 포인터변수 사용
