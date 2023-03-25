833e016702ae9fcf1f7a6cf76b55ef47133f4311
Park YongJune <redplus@redplus.net>
2023-03-25 16:20:25 +0900

    자기 참조 구조체

---
+++ C/C/self_ref_struct.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct ArrayList
+{
+    char name[25];
+    struct ArrayList* next; // 자기 참조 포인터
+} ArrayList;
+
+int main(void)
+{
+    ArrayList* p; // 구조체 포인터 변수
+    ArrayList list1 = { "홍길동" };
+    ArrayList list2 = { "백두산" };
+    ArrayList list3 = { "한라산" };
+
+    list1.next = &list2; // 1~2 구조체 연결
+    list2.next = &list3; // 2~3 구조체 연결
+    list3.next = NULL;   // 구조체 종결(\0)
+
+    p = &list1; // 첫번째 리스트부터 출력
+    while (p != NULL) // 포인터가 널이 아닐 동안 
+    {
+        printf("%s\n", p->name);
+        p = p->next; // 포인터 내용 출력 후 다음 포인터 지정
+    }
+
+    return 0;
+}
