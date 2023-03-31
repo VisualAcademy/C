dc606437b5d079b37599855d71d1865d43c5bf9e
Park YongJune <redplus@redplus.net>
2023-04-01 08:08:01 +0900

    .

---
+++ C/C/string_length_example.c
@@ -0,0 +1,13 @@
+// 문자열 길이 : strlen() : String Length
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    const char* str = " Abc Def Fed Cba ";
+
+    puts(str);
+    printf("문자열 길이 : %zu\n", strlen(str)); // Length는 n - 1 규칙 미적용 
+
+    return 0;
+}
