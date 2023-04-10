a7baaaa9693e1af8a7cb3980d68894c8fd0a0863
Park YongJune <redplus@redplus.net>
2023-04-10 11:15:32 +0900

    .

---
+++ C/C/문자상수.c
@@ -0,0 +1,22 @@
+/*
+    1.7. 예제. 문자상수(Escape Sequence)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("한줄 내림: \n");
+    printf("\n");
+    printf("뒤로 한칸: _\b");
+    printf("\n");
+    printf("탭키 추가: \t");
+    printf("\n");
+    printf("역슬래시 자체: \\");
+    printf("\n");
+    printf("작은따옴표: \' ");
+    printf("\n");
+    printf("큰따옴표: \" ");
+    printf("\n");
+
+    return 0;
+}
