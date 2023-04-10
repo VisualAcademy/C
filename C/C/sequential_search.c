d535bf80b538782398749b12027777748a078adc
Park YongJune <redplus@redplus.net>
2023-04-10 15:56:20 +0900

    .

--- C/C/sequential_search.c
+++ C/C/sequential_search.c
@@ -1,27 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
 
-int main(void) {
+int main(void)
+{
     int i = 0;
     int data[] = { 1, 3, 5, 7, 9, 11 };
     int search = 0;
     int n = sizeof(data) / sizeof(data[0]);
     bool flag = false;
 
-    printf("검색할 정수(1~20) : __\b\b");
+    printf("검색할 정수(1~20): __\b\b");
     scanf("%d", &search);
 
-    for (i = 0; i < n; i++) {
-        if (search == data[i]) {
+    for (i = 0; i < n; i++)
+    {
+        if (search == data[i])
+        {
             flag = true;
         }
     }
 
-    if (flag == true) {
-        printf("%d를 찾았습니다.\n", search);
+    if (flag == true)
+    {
+        printf("%d을(를) 찾았습니다.\n", search);
     }
-    else {
-        printf("%d를 못 찾았습니다.\n", search);
+    else
+    {
+        printf("%d을(를) 못 찾았습니다.\n", search);
     }
 
     return 0;
