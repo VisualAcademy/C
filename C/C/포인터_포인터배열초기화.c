a7d257f4a85322b46cf2dfddd852bf0f6a827e3b
Park YongJune <redplus@redplus.net>
2023-04-13 19:33:56 +0900

    .

---
+++ C/C/포인터_포인터배열초기화.c
@@ -0,0 +1,15 @@
+// 포인터_포인터배열초기화.c
+#include <stdio.h>
+
+int main(void)
+{
+    const char* a[] = { "아빠", "엄마", "아들", "공주" }; 
+    int i;
+
+    for (i = 0; i < 4; i++)
+    {
+        printf("%s\n", a[i]);
+    }
+
+    return 0;
+}
