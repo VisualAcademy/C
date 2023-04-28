bfe76a6cf05c4ccca7ea304214d491cbee3e01c5
Park YongJune <redplus@redplus.net>
2023-04-28 09:12:44 +0900

    .

--- C/기사문제풀이_5회_/기사문제풀이_5회_.c
+++ C/기사문제풀이_5회_/기사문제풀이_5회_.c
@@ -5,11 +5,10 @@ void main(void)
 {
     //[1] Input
     int sum = 0; // 합
-    int data = 1;//1부터 시작해서 인덱스만큼 합을 한 데이터들...
-    int i = 0;
+    int data = 1; // 1부터 시작해서 인덱스만큼 합을 한 데이터들...
 
     //[2] Process
-    for (i = 0; i < 7; i++)
+    for (int i = 0; i < 7; i++)
     {
         if (i % 2 == 0)
         {
@@ -19,13 +18,12 @@ void main(void)
         {
             sum -= data; // 인덱스가 홀수일 때...
         }
-        // printf("%d\n", data);
         data = data + (i + 1);
     }
 
     //[3] Output
-    printf("결과값 : %d\n", sum);//13
-    printf("결과값 : %d\n", (1 - 2 + 4 - 7 + 11 - 16 + 22));//13
+    printf("결괏값 : %d\n", sum);//13
+    printf("결괏값 : %d\n", (1 - 2 + 4 - 7 + 11 - 16 + 22));//13
 
     return 0;
 }
