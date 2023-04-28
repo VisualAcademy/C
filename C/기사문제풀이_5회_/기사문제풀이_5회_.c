aec982a45b13ab3db7b86c35368cb16951890d75
Park YongJune <redplus@redplus.net>
2023-04-28 09:08:55 +0900

    .

--- C/기사문제풀이_5회_/기사문제풀이_5회_.c
+++ C/기사문제풀이_5회_/기사문제풀이_5회_.c
@@ -1,23 +1,31 @@
 // 1-2+4-7+11-16+22 = ?
 #include <stdio.h>
 
-void main() {
-	//[1] Input
-	int sum = 0; // 합
-	int data = 1;//1부터 시작해서 인덱스만큼 합을 한 데이터들...
-	int i = 0;
-	//[2] Process
-	for (i = 0; i < 7; i++) {
-		if (i % 2 == 0) {	// 인덱스가 짝수일 때
-			sum += data;
-		}
-		else {
-			sum -= data;	// 인덱스가 홀수일 때...
-		}
-		// printf("%d\n", data);
-		data = data + (i + 1);
-	}
-	//[3] Output
-	printf("결과값 : %d\n", sum);//13
-	printf("결과값 : %d\n", (1 - 2 + 4 - 7 + 11 - 16 + 22));//13
+void main(void)
+{
+    //[1] Input
+    int sum = 0; // 합
+    int data = 1;//1부터 시작해서 인덱스만큼 합을 한 데이터들...
+    int i = 0;
+
+    //[2] Process
+    for (i = 0; i < 7; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += data; // 인덱스가 짝수일 때
+        }
+        else
+        {
+            sum -= data; // 인덱스가 홀수일 때...
+        }
+        // printf("%d\n", data);
+        data = data + (i + 1);
+    }
+
+    //[3] Output
+    printf("결과값 : %d\n", sum);//13
+    printf("결과값 : %d\n", (1 - 2 + 4 - 7 + 11 - 16 + 22));//13
+
+    return 0;
 }
