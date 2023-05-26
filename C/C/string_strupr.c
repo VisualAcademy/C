fdd303f818c1da2046e19ff19c7fba20dc1f80b9
Park YongJune <redplus@redplus.net>
2023-05-26 19:02:36 +0900

    .

--- C/C/string_strupr.c
+++ C/C/string_strupr.c
@@ -4,12 +4,16 @@
 
 int main(void)
 {
+    // 입력 받을 문자열을 저장할 변수 선언
     char str[80];
 
+    // 사용자에게 문자열을 입력 받음
     printf("문자열을 입력하세요: ");
     scanf("%s", str);
 
-    _strupr(str);
+    _strupr(str);  // _strupr 함수를 사용하여 문자열의 소문자를 대문자로 변환
+
+    // 대문자로 변환된 문자열 출력
     printf("대문자로 변환된 문자열: %s\n", str);
 
     return 0;
