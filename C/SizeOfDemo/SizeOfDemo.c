893c5d61bc99c24567bcd74d08f3e77f6e68fd49
Park YongJune <redplus@redplus.net>
2023-05-29 04:30:12 +0900

    %zu

--- C/SizeOfDemo/SizeOfDemo.c
+++ C/SizeOfDemo/SizeOfDemo.c
@@ -1,17 +1,19 @@
 //[?] sizeof 연산자로 데이터 형식의 크기 구하기
 #include <stdio.h>
 
-main()
+int main() 
 {
 	char c = 'A';
-	int i = 1234; 
+	int i = 1234;
 	long l = 12345;
 	float f = 3.14f;
 	double d = 3.14;
 
-	printf("%d byte\n", sizeof(c)); // 1 byte
-	printf("%d byte\n", sizeof(i)); // 4 byte
-	printf("%d byte\n", sizeof(l)); // 4 byte
-	printf("%d byte\n", sizeof(f)); // 4 byte
-	printf("%d byte\n", sizeof(d)); // 8 byte
+	printf("%zu byte\n", sizeof(c)); // sizeof는 size_t 형을 반환하므로, %zu를 사용해야 합니다.
+	printf("%zu byte\n", sizeof(i));
+	printf("%zu byte\n", sizeof(l));
+	printf("%zu byte\n", sizeof(f));
+	printf("%zu byte\n", sizeof(d));
+
+	return 0; 
 }
