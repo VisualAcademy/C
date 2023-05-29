584cf26879ab02ad93a4e133f3dda8c1cb157e0f
Park YongJune <redplus@redplus.net>
2023-05-29 17:30:18 +0900

    .

--- C/C/union_pointer_access.c
+++ C/C/union_pointer_access.c
@@ -3,29 +3,29 @@
 #include <stdlib.h> // malloc, free 함수가 선언된 헤더 파일
 
 // 공용체 정의
-union Box
+union Container
 {
-    short candy;
-    float snack;
-    int num;
+    short itemCount;
+    float weight;
+    int volume;
 };
 
 int main(void)
 {
-    union Box* b1 = malloc(sizeof(union Box)); // 공용체 포인터 선언, 메모리 할당
+    union Container* c1 = malloc(sizeof(union Container)); // 공용체 포인터 선언, 메모리 할당
 
-    printf("공용체의 전체 크기: %d바이트\n", sizeof(union Box)); // 8: 공용체의 전체 크기는 가장 큰 자료형의 크기
+    printf("공용체의 전체 크기: %d바이트\n", sizeof(union Container)); // 8: 공용체의 전체 크기는 가장 큰 자료형의 크기
 
-    b1->candy = 10;
-    printf("candy의 값: %d\n", b1->candy); // candy의 값: 10
+    c1->itemCount = 10;
+    printf("itemCount의 값: %d\n", c1->itemCount); // itemCount의 값: 10
 
-    b1->snack = 3.14f;
-    printf("snack의 값: %.2f\n", b1->snack); // snack의 값: 3.14
+    c1->weight = 3.14f;
+    printf("weight의 값: %.2f\n", c1->weight); // weight의 값: 3.14
 
-    b1->num = 100;
-    printf("num의 값: %d\n", b1->num); // num의 값: 100
+    c1->volume = 100;
+    printf("volume의 값: %d\n", c1->volume); // volume의 값: 100
 
-    free(b1); // 동적 메모리 해제
+    free(c1); // 동적 메모리 해제
 
     return 0;
 }
