95a9b5232748eb0657ab41c47202faa5968381ae
Park YongJune <redplus@redplus.net>
2023-06-01 17:59:06 +0900

    .

--- C/C/deposit_calculator.c
+++ C/C/deposit_calculator.c
@@ -13,11 +13,11 @@ int main(void)
     printf("기간을 입력하세요: ");
     scanf("%f", &time);
 
-    float interest = principle * pow(1 + rate / 100, time);
-    float amount = principle + interest;
+    float interest = principle * (pow(1 + rate / 100, time) - 1);  // 복리 이자 계산
+    float amount = principle + interest;  // 복리 이자를 더한 총 금액 계산
 
-    printf("이자: %.2f\n", interest);
-    printf("총 금액: %.2f\n", amount);
+    printf("이자: %.2f\n", interest);  // 계산된 이자 출력
+    printf("총 금액: %.2f\n", amount);  // 계산된 총 금액 출력
 
     return 0;
 }
