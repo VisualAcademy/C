4705e35f3b22c377641270d8b4eec237d1c1f064
Park YongJune <redplus@redplus.net>
2023-06-02 01:04:42 +0900

    .

---
+++ C/C/macro_null.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 포인터 ptr을 NULL로 초기화
+    int* ptr = NULL;
+
+    if (ptr == NULL)
+    {
+        printf("포인터는 null입니다.\n");
+    }
+
+    return 0;
+}
