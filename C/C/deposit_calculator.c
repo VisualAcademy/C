dc9e6d6d4277792f5d2a788257be65425f33ed5a
Park YongJune <redplus@redplus.net>
2023-06-01 17:54:28 +0900

    .

--- C/C/deposit_calculator.c
+++ C/C/deposit_calculator.c
@@ -6,18 +6,18 @@ int main(void)
 {
     float principle, rate, time;
 
-    printf("Enter the principle amount: "); // 원금을 입력하세요
+    printf("원금을 입력하세요: ");
     scanf("%f", &principle);
-    printf("Enter the rate of interest: "); // 이자율을 입력하세요
+    printf("이자율을 입력하세요: ");
     scanf("%f", &rate);
-    printf("Enter the time period: "); // 기간을 입력하세요
+    printf("기간을 입력하세요: ");
     scanf("%f", &time);
 
     float interest = principle * pow(1 + rate / 100, time);
     float amount = principle + interest;
 
-    printf("Interest: %.2f\n", interest); // 이자
-    printf("Amount: %.2f\n", amount); // 총 금액
+    printf("이자: %.2f\n", interest);
+    printf("총 금액: %.2f\n", amount);
 
     return 0;
 }
