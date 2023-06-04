418eaf9da4c9ea71627d008ee84bb7573e20ff08
Park YongJune <redplus@redplus.net>
2023-06-05 02:06:22 +0900

    .

--- C/C/file_ftell.c
+++ C/C/file_ftell.c
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
@@ -8,20 +7,23 @@ int main(void)
     char file_name[] = "words.txt";
     int size;
 
-    // 파일 열기
-    fp = fopen(file_name, "r");
+    // 파일 쓰기 모드("w")로 열어 샘플 데이터를 저장
+    fp = fopen(file_name, "w");
     if (fp == NULL)
     {
-        printf("Error opening file %s\n", file_name);
-        exit(1);
+        printf("파일을 열 수 없습니다.\n");
+        return 1;
     }
 
-    // 파일 포인터를 파일 끝으로 옮김
+    // 샘플 데이터를 파일에 씁니다.
+    fprintf(fp, "C Language");
+
+    // 파일 포인터를 파일 끝으로 이동
     fseek(fp, 0, SEEK_END);
 
     // ftell 함수를 사용하여 파일의 크기 구함
     size = ftell(fp);
-    printf("Size of %s is %d bytes\n", file_name, size);
+    printf("파일 %s의 크기는 %d바이트입니다.\n", file_name, size);
 
     // rewind 함수를 사용하여 파일 포인터를 처음으로 돌림
     rewind(fp);
