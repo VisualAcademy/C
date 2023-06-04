d8d204ca6aa53682d28f208c66cd4523e4170a13
Park YongJune <redplus@redplus.net>
2023-06-04 09:58:37 +0900

    .

--- C/C/number_of_words.c
+++ C/C/number_of_words.c
@@ -2,20 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) 
+int main(void)
 {
     char str[100];
     int i, words = 0;
-    printf("Enter a string: ");
+    printf("문자열을 입력하세요: ");
     fgets(str, 100, stdin);
 
-    for (i = 0; str[i] != '\0'; i++) 
+    for (i = 0; str[i] != '\0'; i++)
     {
-        if (str[i] == ' ' || str[i] == '\n') 
+        if (str[i] == ' ' || str[i] == '\n')
         {
             words++;
         }
     }
-    printf("Number of words in the given string: %d\n", words + 1);
+    // 마지막 단어는 '\n'에 의해 증가되므로 1을 빼준다.
+    if (words > 0)
+    {
+        words--;
+    }
+    printf("주어진 문자열에서의 단어 수: %d\n", words + 1);
     return 0;
 }
