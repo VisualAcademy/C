ab3095cf89a78314f6955d8deab3a4d395761f9b
Park YongJune <redplus@redplus.net>
2023-06-05 03:46:41 +0900

    .

--- C/C/file_fopen_fread.c
+++ C/C/file_fopen_fread.c
@@ -7,15 +7,30 @@ int main(void)
     FILE* fp;
     char buffer[SIZE];
 
-    fp = fopen("data.bin", "rb");
+    // data.txt 파일에 샘플 데이터 쓰기
+    fp = fopen("data.txt", "w");
     if (fp == NULL)
     {
-        printf("Error opening file!\n");
+        printf("파일을 열 수 없습니다!\n");
         return 1;
     }
 
-    int items_read = fread(buffer, sizeof(char), SIZE, fp);
-    printf("%d items read from file\n", items_read);
+    fprintf(fp, "Hello, World!\n");
+    fprintf(fp, "C 언어 파일 입출력 예제입니다.\n");
+    fclose(fp);
+
+    // data.txt 파일 읽기
+    fp = fopen("data.txt", "r");
+    if (fp == NULL)
+    {
+        printf("파일을 열 수 없습니다!\n");
+        return 1;
+    }
+
+    while (fgets(buffer, SIZE, fp) != NULL)
+    {
+        printf("%s", buffer);
+    }
     fclose(fp);
 
     return 0;
