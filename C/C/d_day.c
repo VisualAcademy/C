736d848f7276f9abe90afbf48161c5a906e62742
Park YongJune <redplus@redplus.net>
2023-06-12 11:02:30 +0900

    .

--- C/C/d_day.c
+++ C/C/d_day.c
@@ -14,19 +14,15 @@ int main(void)
     current_tm = localtime(&current_time);
 
     // 대상 시간
-    target_tm = (struct tm*)malloc(sizeof(struct tm));
-    target_tm->tm_year = current_tm->tm_year;
-    target_tm->tm_mon = current_tm->tm_mon;
-    target_tm->tm_mday = current_tm->tm_mday + 10; // 10일 후
-    target_time = mktime(target_tm);
+    target_time = current_time + 10 * 24 * 60 * 60;  // 10일 후
+    target_tm = localtime(&target_time);
 
     // 두 시간의 차이 계산
     double diff = difftime(target_time, current_time);
 
     // 결과 출력 (차이는 일 단위)
-    int days = (int)diff / 86400;
+    int days = (int)(diff / 86400);
     printf("%d days\n", days);
 
-    free(target_tm);
     return 0;
 }
