9988ed7f5e1431345e3c7e467c7d4c33b937d401
Park YongJune <redplus@redplus.net>
2023-07-01 14:12:29 +0900

    .

---
+++ C/C/myheader.h
@@ -0,0 +1,8 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+#define MAX_LENGTH 100
+
+int add(int a, int b);
+
+#endif
---
+++ C/C/myheader_main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "myheader.h"
+
+int main(void)
+{
+    int result = add(1, 2);
+    printf("%d\n", result);
+    return 0;
+}
