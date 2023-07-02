2155a9efe993618e1fc07c5005e986600f651a15
Park YongJune <redplus@redplus.net>
2023-07-03 00:32:11 +0900

    .

---
+++ C/C/function_demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// show 함수 선언
+void show(void)
+{
+    // 문자열 출력
+    printf("Hello World\n");
+}
+
+// main 함수
+int main(void)
+{
+    // show 함수 호출
+    show();
+
+    return 0;
+}
