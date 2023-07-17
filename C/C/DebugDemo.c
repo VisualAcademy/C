67ddcd3141c7fda81d9456c5e74c3e6b56ea9a7c
Park YongJune <redplus@redplus.net>
2023-07-17 17:19:06 +0900

    .

---
+++ C/C/DebugDemo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+    int number1 = 10;
+    int number2 = 20;
+    int number3 = 30;
+
+    printf("%d\n", number1);
+    printf("%d\n", number2);
+    printf("%d\n", number3);
+}
