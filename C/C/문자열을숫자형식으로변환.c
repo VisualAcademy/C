42e7e7b01977c8bef33211e2e6e73e933ee1d4dc
Park YongJune <redplus@redplus.net>
2023-07-22 00:06:05 +0900

    .

--- C/C/문자열을숫자형식으로변환.c
+++ C/C/문자열을숫자형식으로변환.c
@@ -1,22 +1,24 @@
 /*
-	문자열을 숫자형식으로 변환
+    문자열을 숫자형식으로 변환
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 
-void main()
+int main(void)
 {
-	char str[] = "3.1415구이6525";
+    char str[] = "3.1415구이6525";
 
-	int i;
-	long l;
-	double d;
+    int i;
+    long l;
+    double d;
 
-	i = atoi(str);
-	l = atol(str);
-	d = atof(str);
+    i = atoi(str);
+    l = atol(str);
+    d = atof(str);
 
-	printf("\na = %d, b = %ld, c = %lf", i, l, d);
-	getch();
+    printf("\na = %d, b = %ld, c = %lf", i, l, d);
+    getch();
+
+    return 0;
 }
