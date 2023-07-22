e669cd0cb97b254c335eaf3f9962be8ccc8e38fe
Park YongJune <redplus@redplus.net>
2023-07-23 01:01:29 +0900

    .

--- C/C/기억클래스_외부변수1.c
+++ C/C/기억클래스_외부변수1.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+
 int x, y;
-void sb1(void) {
+
+void sb1(void)
+{
     printf("[1] 서브 1 : x = %d\n", x++);
 }
-void sb2(void) {
+
+void sb2(void)
+{
     printf("[2] 서브 2 : y = %d\n", y++);
 }
-int main(void) {
+
+int main(void)
+{
     x = 10;
     y = 20;
     sb1();
     sb2();
     sb3();
     sb4();
+
+    return 0;
 }
