fabd7a2ce9aa286d4dfa7343185ea9e570b09959
Park YongJune <redplus@redplus.net>
2023-07-23 01:06:10 +0900

    .

--- C/C/기억클래스_자동변수.c
+++ C/C/기억클래스_자동변수.c
@@ -4,24 +4,33 @@
 static int b;
 int x = 100;
 
-void hello(void) { }
-void hi(void) { b = 10; printf("[3] b = %d\n", b); }
+void hello(void)
+{
+}
 
-int main(void) {
-	//[1] 자동변수 : 기본변수 : 지역(Local) 변수
-	int a; // auto int a;
-	//[3] 레지스터변수 : 캐시메모리에 데이터 저장
-	register int i = 0;
+void hi(void)
+{
+    b = 10;
+    printf("[3] b = %d\n", b);
+}
 
-	a = 10; b = 100;
-	{
-		int a;
-		a = 5;
-		printf("[1] a = %d\n", a);
-	}
-	printf("[2] a = %d\n", a);
-	hi();
-	hello();
+int main(void)
+{
+    //[1] 자동변수 : 기본변수 : 지역(Local) 변수
+    int a; // auto int a;
+    //[3] 레지스터변수 : 캐시메모리에 데이터 저장
+    register int i = 0;
 
-	return 0;
+    a = 10;
+    b = 100;
+    {
+        int a;
+        a = 5;
+        printf("[1] a = %d\n", a);
+    }
+    printf("[2] a = %d\n", a);
+    hi();
+    hello();
+
+    return 0;
 }
