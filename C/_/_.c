285494206513916db0ea3f30ed839a0b5761b361
Park YongJune <redplus@redplus.net>
2023-07-25 01:01:03 +0900

    .

--- C/_/_.c
+++ C/_/_.c
@@ -1,16 +1,19 @@
+/*
+	#define
+	#if ! defined
+	#error
+*/
 #include <stdio.h>
 
-//[1] 매크로 선언
-//#define HI "안녕하세요"
+//매크로 정의
+//#define HELLO "안녕하세요..."
 
-//[2] 매크로 선언 확인
-#if ! defined HI
-#define HI "반갑습니다"
+//매크로 정의되지 않았을 때 에러 발생
+#if ! defined HELLO
+#error "HELLO라는 매크로가 정의되지 않았습니다."
 #endif
 
 int main(void)
 {
-    printf("%s\n", HI);
-
-    return 0;
+	printf("%s\n", HELLO);
 }
