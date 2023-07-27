a2efe76fc5e426da7160f531a8cbc04a3a301b8b
Park YongJune <redplus@redplus.net>
2023-07-27 10:43:50 +0900

    .

---
+++ C/C/매크로_매크로상수의정의취소.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//#define HI "안녕"
+
+#if defined HI		//정의 확인
+#undef HI			//정의 취소
+#define HI "방가"	//재정의
+#else
+#define HI "안녕"	//매크로 정의
+#endif
+
+int main(void)
+{
+	printf("%s \n", HI);
+}
