31351e9cfa83cbe8a7aedb2449f52fe6a2ab47ea
Park YongJune <redplus@redplus.net>
2023-07-28 01:22:10 +0900

    .

---
+++ C/C/매크로_매크로상수정의하기.c
@@ -0,0 +1,17 @@
+/*
+	매크로(Macro) 상수 정의하기
+*/
+#include <stdio.h>
+
+//매크로 정의 : #define
+#define CMain int main(void)
+#define MAX 100
+#define MIN 0
+#define PI 3.141592654
+
+CMain
+{
+	printf("MAX : %d, MIN : %d \n"
+		, MAX, MIN);
+	printf("PI : %f\n", PI);
+}
