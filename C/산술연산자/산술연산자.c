fc890d5f5865dd8d9eb92cbf3ea16e19e6bd9ea0
Park YongJune <redplus@redplus.net>
2023-08-08 19:16:21 +0900

    .

--- C/산술연산자/산술연산자.c
+++ C/산술연산자/산술연산자.c
@@ -6,18 +6,20 @@
 
 int main(void)			// main 함수(메서드) : Entry Point(진입점)
 {
-	//[1] Input(입력)
-	int a = 10;
-	int b = 3;
-	int r = 0;
-	//[2] Process(처리)
-	r = a + b;
-	//[3] Output(출력)
-	printf("%d + %d = %2d\n", a, b, r);
-	printf("%d - %d = %2d\n", a, b, (a - b));
-	printf("%d * %d = %2d\n", a, b, (a * b));
-	printf("%d / %d = %2d\n", a, b, (a / b));
-	printf("%d %% %d = %2d\n", a, b, (a % b)); // a / b = 몫(3), 나머지(%;1)
+    //[1] Input(입력)
+    int a = 10;
+    int b = 3;
+    int r = 0;
+    //[2] Process(처리)
+    r = a + b;
+    //[3] Output(출력)
+    printf("%d + %d = %2d\n", a, b, r);
+    printf("%d - %d = %2d\n", a, b, (a - b));
+    printf("%d * %d = %2d\n", a, b, (a * b));
+    printf("%d / %d = %2d\n", a, b, (a / b));
+    printf("%d %% %d = %2d\n", a, b, (a % b)); // a / b = 몫(3), 나머지(%;1)
 
-	system("pause");
+    //system("pause");
+
+    return 0;
 }
