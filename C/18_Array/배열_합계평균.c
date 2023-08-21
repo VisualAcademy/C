6833f3c60e18893b0746b5e6a2bc9b7af439584f
Park YongJune <redplus@redplus.net>
2023-08-21 11:22:36 +0900

    .

---
+++ C/18_Array/배열_합계평균.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+	int kor[3];
+	int sum;
+	float avg;
+
+	kor[0] = 100;
+	kor[1] = 90;
+	kor[2] = 80;
+
+	sum = kor[0] + kor[1] + kor[2];  // ÃÑÁ¡
+	avg = sum / (float)3.0;			 // Æò±Õ
+
+	printf("ÇÕ°è: %d, Æò±Õ: %.2f\n", sum, avg);
+}
