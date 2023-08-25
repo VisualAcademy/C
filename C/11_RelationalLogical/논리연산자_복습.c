aa562dda1c3939d5b6982e52353999c42f13d2a3
Park YongJune <redplus@redplus.net>
2023-08-25 10:36:44 +0900

    .

---
+++ C/11_RelationalLogical/논리연산자_복습.c
@@ -0,0 +1,23 @@
+/*
+논리 연산자 : &&(AND), ||(OR), !(NOT)
+&& : 둘 다 참일 때에만 참
+|| : 하나라도 참이면 참
+! : 참이면 거짓, 거짓이면 참
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 3; int b = 5; int c = 0;
+
+	c = (a == b) && (a != b);// c = 0 && 1
+	printf("c : %d\n", c);//0(거짓)
+
+	c = (a == b) || (a != b);
+	printf("0 || 1 = %d\n", c);//1(참)
+
+	c = !(a == b); // !0 -> 1
+	printf("!0 = %d\n", c);//1(참)
+
+	system("pause");
+}
