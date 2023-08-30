8ad4c2feeba55d96bc04a350ba88d6d54b5ab883
Park YongJune <redplus@redplus.net>
2023-08-31 04:26:53 +0900

    .

---
+++ C/14_Switch/선택문.c
@@ -0,0 +1,19 @@
+/*
+선택문 : switch~case문
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	int NOW = 17; // Hour
+
+	switch (NOW)
+	{
+	case 1: printf("야심한 시간..."); break;
+	case 2: printf("라면 한그릇..."); break;
+	case 17: printf("하루 정리..."); break;
+	case 21: printf("9시뉴스에 뭐나와요?"); break;
+	default: printf("기타 시간대"); break;
+	}
+	printf("\n");
+}
