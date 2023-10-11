72770e18089eecb98d4dfdeaf65970e15f1fa09d
Park YongJune <redplus@redplus.net>
2023-10-11 09:37:07 +0900

    .

--- C/09_Operator/산술연산자설명.c
+++ C/09_Operator/산술연산자설명.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 // 메인 함수 = f(x)
+// 메인 함수 시작
 int main(void)
 {
 	int i = 10;
@@ -12,15 +13,16 @@ int main(void)
 	printf("%d\n", k); // 15
 
 	k = i - j;
-	printf("%d\n", k); // 15
+	printf("%d\n", k); // 5
 
 	k = i * j;
-	printf("%d\n", k); // 15
+	printf("%d\n", k); // 50
 
 	k = i / j;
-	printf("%d\n", k); // 15
+	printf("%d\n", k); // 2
 
 	k = i % j;
-	printf("%d\n", k); // 15
+	printf("%d\n", k); // 0
 
+	return 0; 
 }
