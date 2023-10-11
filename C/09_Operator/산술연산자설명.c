cedfc9d9925d2f5200a6b0e1aa2655f12376817c
Park YongJune <redplus@redplus.net>
2023-10-11 09:35:59 +0900

    .

---
+++ C/09_Operator/산술연산자설명.c
@@ -0,0 +1,26 @@
+// 산술연산자 : +, -, *, /, %
+#include <stdio.h>
+
+// 메인 함수 = f(x)
+int main(void)
+{
+	int i = 10;
+	int j = 5;
+	int k;
+
+	k = i + j;
+	printf("%d\n", k); // 15
+
+	k = i - j;
+	printf("%d\n", k); // 15
+
+	k = i * j;
+	printf("%d\n", k); // 15
+
+	k = i / j;
+	printf("%d\n", k); // 15
+
+	k = i % j;
+	printf("%d\n", k); // 15
+
+}
