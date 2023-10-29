2e5de4278eb8cd9c5fc5976f59c487a51a88b499
Park YongJune <redplus@redplus.net>
2023-10-30 02:29:24 +0900

    .

--- C/BitAndShift/ShiftOperatorDemo.c
+++ C/BitAndShift/ShiftOperatorDemo.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-main()
+int main(void)
 {
-	int number = 2; // 0010
+    int number = 2; // 0010
 
-	printf("%d\n", number << 1); // 0010 -> 0100 : 4
-	printf("%d\n", number >> 1); // 0010 -> 0001 : 1
+    printf("%d\n", number << 1); // 0010 -> 0100 : 4
+    printf("%d\n", number >> 1); // 0010 -> 0001 : 1
+
+    return 0;
 }
