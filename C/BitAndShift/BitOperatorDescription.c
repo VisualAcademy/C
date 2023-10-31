1ba6902bd0c10076988d076fe9402689691f0700
Park YongJune <redplus@redplus.net>
2023-11-01 02:41:33 +0900

    .

---
+++ C/BitAndShift/BitOperatorDescription.c
@@ -0,0 +1,20 @@
+// 비트 연산자: AND(&), OR(|), XOR(^), NOT(~) 
+#include <stdio.h>
+
+void main()
+{
+	int x = 12; // 1100
+	int y = 10; // 1010
+
+	int a = x & y; // 1000 -> 8
+	int o = x | y; // 1110 -> 14
+	int e = x ^ y; // 0110 -> 6
+	int n = ~x;	   // 11111111...11110011 -> -13
+
+	printf("a: %d\n", a);
+	printf("o: %d\n", o);
+	printf("e: %d\n", e);
+	printf("n: %d\n", n);
+
+	getchar();
+}
