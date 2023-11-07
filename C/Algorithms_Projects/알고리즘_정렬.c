25ea5c889bf312a911ceee467663ecfc6a93ca1e
Park YongJune <redplus@redplus.net>
2023-11-07 10:13:08 +0900

    .

--- C/Algorithms_Projects/알고리즘_정렬.c
+++ C/Algorithms_Projects/알고리즘_정렬.c
@@ -5,29 +5,29 @@
 
 int main(void)
 {
-	//[1] Input
-	int intNum[5] = { 2, 3, 1, 5, 4 };
-	int i, j, temp;
+    //[1] Input
+    int intNum[5] = { 2, 3, 1, 5, 4 };
+    int i, j, temp;
 
-	//[2] Process : Selection Sort(선택 정렬)
-	for (i = 0; i < 5 - 1; i++)
-	{
-		for (j = i + 1; j < 5; j++)
-		{
-			if (intNum[i] > intNum[j])
-			{
-				temp = intNum[i];
-				intNum[i] = intNum[j];
-				intNum[j] = temp;
-			}
-		}
-	}
+    //[2] Process : Selection Sort(선택 정렬)
+    for (i = 0; i < 5 - 1; i++)
+    {
+        for (j = i + 1; j < 5; j++)
+        {
+            if (intNum[i] > intNum[j])
+            {
+                temp = intNum[i];
+                intNum[i] = intNum[j];
+                intNum[j] = temp;
+            }
+        }
+    }
 
-	//[3] Output
-	for (i = 0; i < 5; i++)
-	{
-		printf("%d\n", intNum[i]);
-	}
+    //[3] Output
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d\n", intNum[i]);
+    }
 
-	return 0; 
+    return 0;
 }
