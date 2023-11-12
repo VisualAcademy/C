f3f93429a46128b0ad813822eed73ca0f58edf39
Park YongJune <redplus@redplus.net>
2023-11-13 00:12:50 +0900

    .

---
+++ C/Algorithms_Projects/반올림.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //[1] Input
+    double dblNum = 3.777;
+
+    //[2] Process
+    dblNum = (int)((dblNum + 0.05) * 10) / 10.0;
+
+    //[3] Output
+    printf("¹Ý¿Ã¸²: %.2f\n", dblNum);//3.80
+
+    return 0;
+}
