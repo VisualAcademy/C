1086106808e1af92d96c0092059299f597d7fed5
Park YongJune <redplus@redplus.net>
2023-11-19 02:01:22 +0900

    .

---
+++ C/Algorithms_Projects/기사문제풀이_2회.c
@@ -0,0 +1,28 @@
+// 1부터 1000까지 홀수의 합 : ?
+// 1부터 1000까지 짝수의 합 : ?
+#include <stdio.h>
+
+void main()
+{
+	//[1] Input
+	int intSum1 = 0;//홀수합
+	int intSum2 = 0;//짝수합
+	int i;
+	//[2] Process
+	i = 1;					//초기식
+	do
+	{
+		if (i % 2 != 0)		//필터링+실행문
+		{
+			intSum1 += i;
+		}
+		else
+		{
+			intSum2 += i;
+		}
+		i++;				//증감식
+	} while (i <= 1000);		//조건식
+	//[3] Output
+	printf("홀수합 : %d\n", intSum1);
+	printf("짝수합 : %d\n", intSum2);
+}
