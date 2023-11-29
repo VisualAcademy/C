0c81cad457d22bb10d56789953193287992bc745
Park YongJune <redplus@redplus.net>
2023-11-30 03:12:19 +0900

    .

---
+++ C/Algorithms_Projects/순차검색알고리즘설명.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//[1] Input
+	int intNum[] = { 5,4,3,2,1 };
+	int intSearch;
+	int i;
+
+	//[2] Process
+	puts("찾을 데이터 : "); scanf("%d", &intSearch);
+	for (i = 0; i < 5; i++)         //모든 데이터 반복
+	{
+		if (intNum[i] == intSearch) //비교
+		{
+			printf("찾았습니다.");
+			break;
+		}
+	}
+
+	return 0;
+}
