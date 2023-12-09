d5fe5d13687ee66ae5620818fe91945717f73083
Park YongJune <redplus@redplus.net>
2023-12-09 16:06:02 +0900

    .

--- C/c-language/알고리즘_최소값.c
+++ C/c-language/알고리즘_최소값.c
@@ -3,24 +3,24 @@
 
 int main(void)
 {
-	//[1] Input
-	int intNum[5] = { -33, -22, -44, -5, -11 };
-	int intMin = INT_MAX;//정수형이 가질 수 있는 가장 큰 값
-	int i = 0;
+    //[1] Input
+    int intNum[5] = { -33, -22, -44, -5, -11 };
+    int intMin = INT_MAX; // 정수형이 가질 수 있는 가장 큰 값
+    int i = 0;
 
-	//[2] Process : 최솟값(MIN) 알고리즘
-	i = 0;
-	while (i < 5)
-	{
-		if (intMin > intNum[i])
-		{
-			intMin = intNum[i];
-		}
-		i++;
-	}
+    //[2] Process: 최솟값(MIN) 알고리즘
+    i = 0;
+    while (i < 5)
+    {
+        if (intMin > intNum[i])
+        {
+            intMin = intNum[i];
+        }
+        i++;
+    }
 
-	//[3] Output
-	printf("최솟값 : %d\n", intMin);// -44
+    //[3] Output
+    printf("최솟값 : %d\n", intMin); // -44
 
-	return 0;
+    return 0;
 }
